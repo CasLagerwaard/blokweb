915be514b4f175b0d5d5378ffbcfd9c6f0417e72
CasLagerwaard <70697670+CasLagerwaard@users.noreply.github.com>
2020-10-25 13:46:14 +0100

    Add files via upload

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,1 +1,8 @@
 // JavaScript Document
+var burgermenu = document.querySelector('nav ul');
+var burgerbutton = document.querySelector('header nav button:first-of-type');
+
+function toggle() {
+    burgermenu.classList.toggle("hamburgermenu");
+}
+burgerbutton.addEventListener('click', toggle);
