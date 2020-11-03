0fd5cf3e2e575d23ae282fc9083abdbbadb5f042
CasLagerwaard <70697670+CasLagerwaard@users.noreply.github.com>
2020-11-03 14:20:18 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+var burgermenu = document.querySelector('nav ul');
+var burgerbutton = document.querySelector('header nav button:first-of-type');
+
+function toggleburger() {
+    burgermenu.classList.toggle("hamburgermenu");
+}
+burgerbutton.addEventListener('click', toggleburger);
+
+
+
+
+var footer = document.querySelector('footer ul');
+var footerbutton = document.querySelector('footer button');
+
+function togglefooter() {
+    footer.classList.toggle("footeraan");
+}
+footerbutton.addEventListener('click', togglefooter);
